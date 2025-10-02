5a136742719ea5bed3a5de913cdf10549a0123cd
patricklarocque1 <patrick.adrian1214@gmail.com>
2025-10-02 00:09:34 -0400

    Upgrade Java version to 21 across all modules; update compile options and JVM toolchain settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -104,8 +104,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     buildFeatures {
@@ -135,7 +135,7 @@ android {
 }
 
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(21)
 }
 
 dependencies {
--- macrobenchmark/build.gradle.kts
+++ macrobenchmark/build.gradle.kts
@@ -44,8 +44,8 @@ android {
     experimentalProperties["android.experimental.self-instrumenting"] = true
     
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     buildFeatures {
@@ -54,7 +54,7 @@ android {
 }
 
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(21)
 }
 
 dependencies {
